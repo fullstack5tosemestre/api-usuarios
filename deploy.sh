13faa9896fdbc7cebd00a23c0c177226c3e17d4d
dainz <mbenjaminarancibia@gmail.com>
2026-05-13 11:44:32 -0400

    remove unused files

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-docker run -d \
-    --rm \
-    -p 8082:8082 \
-    --env-file .env \
-    xdainz/api-usuarios:latest
--- run_db.sh
+++
@@ -1,6 +0,0 @@
-docker run -d \
-    --name db_usuario \
-    -e MYSQL_ROOT_PASSWORD=root \
-    -e MYSQL_DATABASE=db_usuario \
-    -p 3308:3306 \
-    mysql:8.0
